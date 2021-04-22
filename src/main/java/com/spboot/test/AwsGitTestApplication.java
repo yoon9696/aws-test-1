5933d8cd5cfd4deaee158df62244b5048bdbc21b
Administrator <Administrator@192.168.219.104>
2021-04-22 12:57:30 +0900

    123

---
+++ src/main/java/com/spboot/test/AwsGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.spboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsGitTestApplication.class, args);
+	}
+
+}
